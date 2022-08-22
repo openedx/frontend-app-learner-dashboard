5ce3fbb8d77a1387cae172f3c32eeea2e3b3985b
Leangseu Kim <lkim@edx.org>
2022-08-22 10:42:19 -0400

    fix: course card banner radius

--- src/containers/CourseCard/index.jsx
+++ src/containers/CourseCard/index.jsx
@@ -17,41 +17,47 @@ import CourseCardActions from './components/CourseCardActions';
 import messages from './messages';
 import CourseCardDetails from './components/CourseCardDetails';
 
+import './CourseCard.scss';
+
 export const CourseCard = ({ cardId }) => {
   const {
-    isEnrolled,
-    title,
-    bannerUrl,
-    formatMessage,
-  } = useCardData({ cardId });
+    isEnrolled, title, bannerUrl, formatMessage,
+  } = useCardData({
+    cardId,
+  });
   return (
     <div className="mb-4.5 course-card" data-testid="CourseCard">
       <Card orientation="horizontal">
-        <Card.ImageCap
-          src={bannerUrl}
-          srcAlt={formatMessage(messages.bannerAlt)}
-        />
-        <Card.Body>
-          <Card.Header
-            title={<span data-testid="CourseCardTitle">{title}</span>}
-            actions={<CourseCardMenu cardId={cardId} />}
-          />
-          <Card.Section>
-            <CourseCardDetails cardId={cardId} />
-          </Card.Section>
-          <Card.Footer
-            orientation="vertical"
-            textElement={<RelatedProgramsBadge cardId={cardId} />}
-          >
-            <CourseCardActions cardId={cardId} />
-          </Card.Footer>
-        </Card.Body>
+        <div className="d-flex flex-column w-100">
+          <div className="d-flex">
+            <Card.ImageCap
+              src={bannerUrl}
+              srcAlt={formatMessage(messages.bannerAlt)}
+            />
+            <Card.Body>
+              <Card.Header
+                title={<span data-testid="CourseCardTitle">{title}</span>}
+                actions={<CourseCardMenu cardId={cardId} />}
+              />
+              <Card.Section>
+                <CourseCardDetails cardId={cardId} />
+              </Card.Section>
+              <Card.Footer
+                orientation="vertical"
+                textElement={<RelatedProgramsBadge cardId={cardId} />}
+              >
+                <CourseCardActions cardId={cardId} />
+              </Card.Footer>
+            </Card.Body>
+          </div>
+
+          <div className="course-card-banners" data-testid="CourseCardBanners">
+            <CourseBanner cardId={cardId} />
+            <EntitlementBanner cardId={cardId} />
+            {isEnrolled && <CertificateBanner cardId={cardId} />}
+          </div>
+        </div>
       </Card>
-      <div className="course-card-banners" data-testid="CourseCardBanners">
-        <CourseBanner cardId={cardId} />
-        <EntitlementBanner cardId={cardId} />
-        {isEnrolled && <CertificateBanner cardId={cardId} />}
-      </div>
     </div>
   );
 };
