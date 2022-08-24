368bb5e71792e21b1f1afb2e9f11384f9ebc3699
Ben Warzeski <bwarzeski@edx.org>
2022-08-24 14:30:06 -0400

    fix: api url had extra slash

--- src/data/services/lms/urls.js
+++ src/data/services/lms/urls.js
@@ -4,7 +4,7 @@ import { configuration } from 'config';
 const baseUrl = `${configuration.LMS_BASE_URL}`;
 
 const api = `${baseUrl}/api/`;
-const init = `${api}/learner_home/mock/init`;
+const init = `${api}learner_home/mock/init`;
 
 export default StrictDict({
   api,
