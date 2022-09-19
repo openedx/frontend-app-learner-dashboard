15cb385cc6c26be838742e13bd2029b41410474a
Ben Warzeski <bwarzeski@edx.org>
2022-09-19 10:46:24 -0400

    feat: add unenroll and update email settings api events

--- src/data/services/lms/api.js
+++ src/data/services/lms/api.js
@@ -1,3 +1,5 @@
+import { handleEvent } from 'data/services/segment/utils';
+import { eventNames } from 'data/services/segment/constants';
 import {
   client,
   get,
@@ -21,8 +23,20 @@ const deleteEntitlementEnrollment = ({ uuid }) => client().delete(stringifyUrl(
   { course_run_id: null },
 ));
 
+const updateEmailSettings = ({ courseId, enable }) => post(stringifyUrl(
+  urls.updateEmailSettings,
+  { course_id: courseId, ...(enable && { receive_emails: 'on' }) },
+));
+
+const unenrollFromCourse = ({ courseId }) => post(stringifyUrl(
+  urls.unenrollFromCourse,
+  { course_id: courseId, enrollment_action: 'unenroll' },
+));
+
 export default {
   initializeList,
+  unenrollFromCourse,
+  updateEmailSettings,
   updateEntitlementEnrollment,
   deleteEntitlementEnrollment,
 };
