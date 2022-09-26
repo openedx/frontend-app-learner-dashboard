03107a282b6e4064d116c8e9b74d06526d10ff8b
Ben Warzeski <bwarzeski@edx.org>
2022-09-26 10:14:14 -0400

    fix: urls

--- src/data/services/lms/urls.js
+++ src/data/services/lms/urls.js
@@ -5,10 +5,10 @@ const baseUrl = `${configuration.LMS_BASE_URL}`;
 
 const api = `${baseUrl}/api/`;
 
-const init = `${api}learner_home/mock/init`; // mock endpoint for testing
-// const init = `${api}learner_home/init`;
+// const init = `${api}learner_home/mock/init`; // mock endpoint for testing
+const init = `${api}learner_home/init`;
 
-const courseUnenroll = `${api}/courses/unenroll`; // TODO: Fix
+const courseUnenroll = `${baseUrl}/change_enrollment`;
 const updateEmailSettings = `${api}/change_email_settings`;
 const entitlementEnrollment = (uuid) => `${api}/entitlements/v1/entitlements/${uuid}/enrollments`;
 
