faa4bff409ab3daf04da97ab27ea18a50eba49ca
Leangseu Kim <lkim@edx.org>
2022-10-17 13:40:48 -0400

    chore: update url

--- src/data/services/lms/urls.js
+++ src/data/services/lms/urls.js
@@ -3,10 +3,10 @@ import { configuration } from 'config';
 
 const baseUrl = `${configuration.LMS_BASE_URL}`;
 
-const api = `${baseUrl}/api/`;
+const api = `${baseUrl}/api`;
 
 // const init = `${api}learner_home/mock/init`; // mock endpoint for testing
-const init = `${api}learner_home/init`;
+const init = `${api}/learner_home/init`;
 
 const courseUnenroll = `${baseUrl}/change_enrollment`;
 const updateEmailSettings = `${api}/change_email_settings`;
