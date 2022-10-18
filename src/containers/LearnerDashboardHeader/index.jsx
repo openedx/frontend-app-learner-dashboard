4fac404c6a080b7588bb0e7c8496c490412b7ea6
Leangseu Kim <lkim@edx.org>
2022-10-18 18:30:54 -0400

    chore: give masquerade bar box shadow

--- src/containers/LearnerDashboardHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/index.jsx
@@ -53,7 +53,6 @@ export const LearnerDashboardHeader = () => {
         {!isCollapsed && <GreetingBanner size="large" />}
       </div>
       <MasqueradeBar />
-      <hr className="mt-0" />
     </>
   );
 };
