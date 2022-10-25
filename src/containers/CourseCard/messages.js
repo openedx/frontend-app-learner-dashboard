f8b181e8c9bd4184c19e7481a8b3327b22cbbb2c
Leangseu Kim <lkim@edx.org>
2022-10-25 13:33:26 -0400

    feat: add verified ribbon

--- src/containers/CourseCard/messages.js
+++ src/containers/CourseCard/messages.js
@@ -6,6 +6,21 @@ export const messages = StrictDict({
     description: 'Course card banner alt-text',
     defaultMessage: 'Course thumbnail',
   },
+  verifiedBanner: {
+    id: 'learner-dash.courseCard.verifiedBanner',
+    description: 'Course card verified banner',
+    defaultMessage: 'Verified',
+  },
+  verifiedHoverDescription: {
+    id: 'learner-dash.courseCard.verifiedHoverDescription',
+    description: 'Course card verified hover description',
+    defaultMessage: 'You\'re enrolled as a verified student',
+  },
+  verifiedBannerRibbonAlt: {
+    id: 'learner-dash.courseCard.verifiedBannerRibbonAlt',
+    description: 'Course card verified banner ribbon alt-text',
+    defaultMessage: 'ID Verified Ribbon/Badge',
+  },
 });
 
 export default messages;
