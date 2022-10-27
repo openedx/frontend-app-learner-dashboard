7744e435e6a3e18fbc997909f6c2bd7fb7a77cf8
Leangseu Kim <lkim@edx.org>
2022-10-27 16:21:01 -0400

    chore: add empty case to course search url

--- src/containers/WidgetSidebar/widgets/LookingForChallengeWidget/index.test.jsx
+++ src/containers/WidgetSidebar/widgets/LookingForChallengeWidget/index.test.jsx
@@ -1,18 +1,27 @@
 import { shallow } from 'enzyme';
 
+import { hooks } from 'data/redux';
+
 import LookingForChallengeWidget from '.';
 
 jest.mock('data/redux', () => ({
   hooks: {
-    usePlatformSettingsData: () => ({
-      courseSearchUrl: 'course-search-url',
-    }),
+    usePlatformSettingsData: jest.fn(),
   },
 }));
 
 describe('LookingForChallengeWidget', () => {
   describe('snapshots', () => {
-    test('default', () => {
+    test('no course search url', () => {
+      hooks.usePlatformSettingsData.mockReturnValueOnce({});
+      const wrapper = shallow(<LookingForChallengeWidget />);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('with course-search-url', () => {
+      hooks.usePlatformSettingsData.mockReturnValueOnce({
+        courseSearchUrl: 'course-search-url',
+      });
       const wrapper = shallow(<LookingForChallengeWidget />);
       expect(wrapper).toMatchSnapshot();
     });
