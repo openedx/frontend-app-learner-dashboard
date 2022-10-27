758e81748ea5f7b035d16faa1a219b2e7fa62a38
Leangseu Kim <lkim@edx.org>
2022-10-27 15:59:22 -0400

    chore: ensure twitter button work even on the edge

--- src/containers/CourseCard/components/CourseCardMenu/index.jsx
+++ src/containers/CourseCard/components/CourseCardMenu/index.jsx
@@ -71,18 +71,17 @@ export const CourseCardMenu = ({ cardId }) => {
             )}
           */}
           {twitter.isEnabled && (
-            <Dropdown.Item>
               <ReactShare.TwitterShareButton
                 url={twitter.shareUrl}
                 title={formatMessage(messages.shareQuote, {
                   courseName,
                   socialBrand: twitter.socialBrand,
                 })}
-                className="w-100 text-left"
+                resetButtonStyle={false}
+                className="pgn__dropdown-item dropdown-item"
               >
                 {formatMessage(messages.shareToTwitter)}
               </ReactShare.TwitterShareButton>
-            </Dropdown.Item>
           )}
         </Dropdown.Menu>
       </Dropdown>
