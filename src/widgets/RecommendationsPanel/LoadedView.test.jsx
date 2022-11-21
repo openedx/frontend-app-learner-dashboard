5eab0f0b68d2159c95f1ad0459ba351ef1329d55
Leangseu Kim <lkim@edx.org>
2022-11-21 15:42:36 -0500

    test: add unit test

--- src/widgets/RecommendationsPanel/LoadedView.test.jsx
+++ src/widgets/RecommendationsPanel/LoadedView.test.jsx
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import LoadedView from './LoadedView';
 import mockData from './mockData';
+import messages from './messages';
 
 jest.mock('./components/CourseCard', () => 'CourseCard');
 jest.mock('data/redux', () => ({
@@ -19,7 +20,16 @@ describe('RecommendationsPanel LoadedView', () => {
     isPersonalizedRecommendation: false,
     courseSearchClickTracker: jest.fn().mockName('courseSearchClickTracker'),
   };
-  test('snapshot', () => {
-    expect(shallow(<LoadedView {...props} />)).toMatchSnapshot();
+  describe('snapshot', () => {
+    test('without personalize recommendation', () => {
+      const el = shallow(<LoadedView {...props} />);
+      expect(el).toMatchSnapshot();
+      expect(el.find('h3').text()).toEqual(messages.popularCoursesHeading.defaultMessage);
+    });
+    test('with personalize recommendation', () => {
+      const el = shallow(<LoadedView {...props} isPersonalizedRecommendation />);
+      expect(el).toMatchSnapshot();
+      expect(el.find('h3').text()).toEqual(messages.recommendationsHeading.defaultMessage);
+    });
   });
 });
