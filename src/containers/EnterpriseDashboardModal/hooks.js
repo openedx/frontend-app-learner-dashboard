81f1282cb05775ca22bf4e4bc5adc3c63ebed3c0
Ben Warzeski <bwarzeski@edx.org>
2022-12-15 10:53:04 -0500

    fix: only fire event if dashboard actually opens (#97)

--- src/containers/EnterpriseDashboardModal/hooks.js
+++ src/containers/EnterpriseDashboardModal/hooks.js
@@ -30,7 +30,11 @@ export const useEnterpriseDashboardHook = () => {
     setShowModal(false);
   };
 
-  React.useEffect(trackOpened, []);
+  React.useEffect(() => {
+    if (dashboard && dashboard.label) {
+      trackOpened();
+    }
+  }, []); // eslint-disable-line
 
   return {
     showModal,
