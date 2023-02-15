38b0d5832f9fd4a53301f4e08867389db9855866
attiyaishaque <atiya.ishaq@arbisoft.com>
2023-02-15 14:00:07 +0500

    fix: [VAN-1292] Remove personalized recommendation cookie

--- src/widgets/RecommendationsPanel/components/hooks.js
+++ src/widgets/RecommendationsPanel/components/hooks.js
@@ -1,28 +1,9 @@
-import { configuration } from 'config';
-import { setCookie, getCookie } from 'utils/cookies';
-
 import track from '../track';
 import './index.scss';
 
 export const useCourseCardData = (course, isControl) => {
   const handleCourseClick = (e) => {
     e.preventDefault();
-
-    const cookieName = configuration.PERSONALIZED_RECOMMENDATION_COOKIE_NAME;
-    let recommendedCourses = getCookie(cookieName);
-    if (typeof recommendedCourses === 'undefined') {
-      recommendedCourses = { course_keys: [course.courseKey] };
-    } else if (!recommendedCourses.course_keys.includes(course.courseKey)) {
-      if (recommendedCourses.course_keys.length < 5) {
-        recommendedCourses.course_keys.push(course.courseKey);
-      } else {
-        recommendedCourses.course_keys.shift();
-        recommendedCourses.course_keys.push(course.courseKey);
-      }
-    }
-    recommendedCourses.is_control = isControl;
-    recommendedCourses.is_personalized_recommendation = isControl;
-    setCookie(cookieName, JSON.stringify(recommendedCourses), 365);
 
     track.recommendedCourseClicked(
       course.courseKey,
