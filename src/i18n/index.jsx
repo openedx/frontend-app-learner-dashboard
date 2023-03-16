50a540b3b749cfdbd4898493f7bcb858eede7fb8
Leangseu Kim <lkim@edx.org>
2023-03-16 14:04:20 -0400

    chore: add es, fr_ca, pt_BR translation
    chore: remove intl as it is no longer needed
    
    chore: update test snaphot

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -2,12 +2,16 @@ import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
+import frcaMessages from './messages/fr_ca.json';
+import ptbrMessages from './messages/pt_BR.json';
 import zhcnMessages from './messages/zh_CN.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
+  'fr-ca': frcaMessages,
+  'pt-br': ptbrMessages,
   'zh-cn': zhcnMessages,
 };
 
