5dd00e9f24b7a7684093248a8ab2fbb1e0287f4c
Mubbshar Anwar <78487564+mubbsharanwar@users.noreply.github.com>
2023-04-06 17:39:27 +0500

    refactor: update API endpoint (#129)

--- src/widgets/RecommendationsPanel/api.js
+++ src/widgets/RecommendationsPanel/api.js
@@ -2,7 +2,7 @@ import { StrictDict } from 'utils';
 import { get, stringifyUrl } from 'data/services/lms/utils';
 import urls from 'data/services/lms/urls';
 
-export const fetchUrl = `${urls.api}/learner_home/recommendation/courses/`;
+export const fetchUrl = `${urls.api}/learner_recommendations/courses/`;
 export const apiKeys = StrictDict({ user: 'user' });
 
 const fetchRecommendedCourses = () => get(stringifyUrl(fetchUrl));
