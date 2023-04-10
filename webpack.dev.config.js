0651b5f496ea4fe9acd7a09fbcec0cc02d9c5d74
Leangseu Kim <lkim@edx.org>
2023-04-10 15:03:39 -0400

    chore: add copy to dev for stage-dev build

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { createConfig } = require('@edx/frontend-build');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const config = createConfig('webpack-dev');
 
@@ -9,5 +10,16 @@ config.resolve.modules = [
 ];
 
 config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
+
+config.plugins.push(
+  new CopyPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, './public/robots.txt'),
+        to: path.resolve(__dirname, './dist/robots.txt'),
+      },
+    ],
+  }),
+);
 
 module.exports = config;
