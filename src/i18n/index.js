576962925041b2cc83c953635536eea1a7cb840c
Omar Al-Ithawi <i@omardo.com>
2023-05-09 17:05:48 +0300

    feat: use `atlas` in `make pull_translations` (#137)
     - Bump frontend-platform to bring intl-imports.js script
     - Move all i18n imports into `src/i18n/index.js` so intl-imports.js can override it with latest translations
     - Add `atlas` into `make pull_translations` when `OPENEDX_ATLAS_PULL` environment variable is set.
    
    This pull request is part of the [FC-0012 project](https://openedx.atlassian.net/l/cp/XGS0iCcQ) which is sparked by the [Translation Infrastructure update OEP-58](https://open-edx-proposals.readthedocs.io/en/latest/architectural-decisions/oep-0058-arch-translations-management.html#specification).

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,3 +1,5 @@
+import { messages as footerMessages } from '@edx/frontend-component-footer';
+
 import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
@@ -6,7 +8,7 @@ import frcaMessages from './messages/fr_CA.json';
 import ptbrMessages from './messages/pt_BR.json';
 import zhcnMessages from './messages/zh_CN.json';
 
-const messages = {
+const appMessages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
@@ -15,4 +17,7 @@ const messages = {
   'zh-cn': zhcnMessages,
 };
 
-export default messages;
+export default [
+  footerMessages,
+  appMessages,
+];
