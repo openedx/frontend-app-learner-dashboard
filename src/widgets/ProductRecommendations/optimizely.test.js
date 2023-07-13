e959305db7bb1f2dd0ec02b9cad5c3f87ef46159
Jody Bailey <jbailey@2u.com>
2023-07-13 11:11:31 +0200

    fix: removed info Optimizely logs

--- src/widgets/ProductRecommendations/optimizely.test.js
+++ src/widgets/ProductRecommendations/optimizely.test.js
@@ -1,13 +1,15 @@
-import { createInstance } from '@optimizely/react-sdk';
+import { createInstance, setLogLevel } from '@optimizely/react-sdk';
 import optimizelyClient from './optimizely';
 
 jest.mock('@optimizely/react-sdk', () => ({
   createInstance: jest.fn(() => 'mockedClient'),
+  setLogLevel: jest.fn(),
 }));
 
 describe('optimizelyClient', () => {
-  it('should create an Optimizely client instance with the correct SDK key', () => {
+  it('should configure an Optimizely client instance with the correct SDK key', () => {
     expect(optimizelyClient).toBeDefined();
+    expect(setLogLevel).toHaveBeenCalledWith('error');
     expect(createInstance).toHaveBeenCalledWith({ sdkKey: 'SDK Key' });
   });
 });
