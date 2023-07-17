ed2368222fb72967d8a996f035b9f132a5405787
Jody Bailey <110463597+JodyBaileyy@users.noreply.github.com>
2023-07-17 14:57:57 +0200

    fix: removed info Optimizely logs (#177)

--- src/widgets/ProductRecommendations/optimizely.js
+++ src/widgets/ProductRecommendations/optimizely.js
@@ -1,9 +1,15 @@
-import { createInstance } from '@optimizely/react-sdk';
+import { createInstance, setLogLevel } from '@optimizely/react-sdk';
 
 const OPTIMIZELY_SDK_KEY = process.env.OPTIMIZELY_FULL_STACK_SDK_KEY;
 
-const optimizelyClient = createInstance({
-  sdkKey: OPTIMIZELY_SDK_KEY,
-});
+const configureClient = () => {
+  setLogLevel('error');
+
+  return createInstance({
+    sdkKey: OPTIMIZELY_SDK_KEY,
+  });
+};
+
+const optimizelyClient = configureClient();
 
 export default optimizelyClient;
