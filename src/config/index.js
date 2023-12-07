d5c5bc4f31de4c552b4949ea475197fcda0afd31
Maria Grimaldi <maria.grimaldi@edunext.co>
2023-12-07 01:00:57 -0400

    refactor: turn off personal dashboard by default

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,7 @@ const configuration = {
   ENABLE_NOTICES: process.env.ENABLE_NOTICES || null,
   CAREER_LINK_URL: process.env.CAREER_LINK_URL || null,
   LOGO_URL: process.env.LOGO_URL,
-  ENABLE_EDX_PERSONAL_DASHBOARD: process.env.ENABLE_EDX_PERSONAL_DASHBOARD || null,
+  ENABLE_EDX_PERSONAL_DASHBOARD: process.env.ENABLE_EDX_PERSONAL_DASHBOARD == 'true',
 };
 
 const features = {};
