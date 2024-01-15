c73c0fe30dbe8c7afed7ea98ee99851f43485b7e
hinakhadim <hina.khadim@arbisoft.com>
2024-01-15 13:31:59 +0500

    fix: URLs get the current value of LMS_BASE_URL from getConfig() #269

--- src/containers/LearnerDashboardHeader/ExpandedHeader/index.test.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/index.test.jsx
@@ -5,7 +5,7 @@ import ExpandedHeader from '.';
 import { useIsCollapsed } from '../hooks';
 
 jest.mock('data/services/lms/urls', () => ({
-  programsUrl: 'programsUrl',
+  programsUrl: () => 'programsUrl',
   baseAppUrl: url => (`http://localhost:18000${url}`),
 }));
 
