dc2ae4027e4c1f8b6cc99146218cb053afae529e
Ahtesham Quraish <ahtesham.quraish@A006-001323.local>
2024-04-25 15:37:27 +0500

    fix: replace the header with openedx header Description: Replace the header with openedx header van-1914

--- src/containers/LearnerDashboardHeader/hooks.test.js
+++ src/containers/LearnerDashboardHeader/hooks.test.js
@@ -13,6 +13,7 @@ const {
   findCoursesNavClicked,
   findCoursesNavDropdownClicked,
   useLearnerDashboardHeaderData,
+  useLearnerDashboardHeaderMenu,
 } = hooks;
 
 jest.mock('tracking', () => ({
@@ -45,6 +46,17 @@ describe('LearnerDashboardHeader hooks', () => {
       expect(track.findCourses.findCoursesClicked).toHaveBeenCalledWith(url, {
         linkName: linkNames.learnerHomeNavExplore,
       });
+    });
+  });
+
+  describe('getLearnerDashboardHeaderMenu', () => {
+    test('calls header menu data hook', () => {
+      const courseSearchUrl = '/courses';
+      const authenticatedUser = {
+        username: 'test',
+      };
+      const learnerHomeHeaderMenu = useLearnerDashboardHeaderMenu({ courseSearchUrl, authenticatedUser });
+      expect(learnerHomeHeaderMenu.mainMenu.length).toBe(3);
     });
   });
 
