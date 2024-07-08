59807ebbf2a6af79a8878731ae21124501eace91
Jason Wesson <jsnwesson@gmail.com>
2024-07-08 14:12:08 -0700

    Revert "update 2u/main from master (#378)"
    This reverts commit 5093527acb6c0c13d9e5afa7ded0e294fa28e1ab.

--- src/widgets/RecommendationsPanel/api.js
+++ src/widgets/RecommendationsPanel/api.js
@@ -2,7 +2,7 @@ import { StrictDict } from 'utils';
 import { get, stringifyUrl } from 'data/services/lms/utils';
 import urls from 'data/services/lms/urls';
 
-export const getFetchUrl = () => (`${urls.getApiUrl()}/edx_recommendations/learner_dashboard/course_recommendations/`);
+export const getFetchUrl = () => (`${urls.getApiUrl()}/edx_recommendations/learner_dashboard/amplitude/`);
 export const apiKeys = StrictDict({ user: 'user' });
 
 const fetchRecommendedCourses = () => get(stringifyUrl(getFetchUrl()));
