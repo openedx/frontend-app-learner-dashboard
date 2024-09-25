2a8a4017b680914b4049b8f6ee4231f1d56db11f
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-09-25 12:40:44 +0700

    update

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,19 +4,25 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const config = createConfig('webpack-prod');
 
+// Resolving modules from src and node_modules
 config.resolve.modules = [
   path.resolve(__dirname, './src'),
   'node_modules',
 ];
 
+// Excluding certain node_modules from the build
 config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
 
+// Add CopyPlugin to copy all assets from the public folder
 config.plugins.push(
   new CopyPlugin({
     patterns: [
       {
-        from: path.resolve(__dirname, './public/robots.txt'),
-        to: path.resolve(__dirname, './dist/robots.txt'),
+        from: path.resolve(__dirname, './public'), // Copy entire public folder
+        to: path.resolve(__dirname, './dist/public'), // Destination folder for public assets
+        globOptions: {
+          ignore: ['**/robots.txt'], // You can exclude specific files if needed
+        },
       },
     ],
   }),
