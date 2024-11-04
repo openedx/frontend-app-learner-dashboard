c9540e7cf32fbdf02456bcec393f14aee59cfc5f
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-11-05 00:03:40 +0700

    update footer static links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {
   faFacebookF,
-  faInstagram,
   faLinkedin,
+  faTelegram,
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -72,11 +72,12 @@ const Footer = () => {
       {/* Bottom Footer */}
       <div className="footer-bottom font-red-hat-display">
         <Container fluid>
-          <Row className="text-center text-white text-md-left">
+          <Row className="text-left text-white text-md-left">
             {/* Logo and Social Media */}
             <Col
               xs={12}
-              md={4}
+              md={12}
+              lg={3}
               className="mb-4 mb-md-0 d-flex flex-column align-items-center"
             >
               <img
@@ -89,23 +90,23 @@ const Footer = () => {
                 style={{ maxWidth: '300px' }}
               >
                 <li className="list-inline-item  border-green rounded-circle p-2">
-                  <a href="#">
+                  <a href="https://www.facebook.com/creditbureaucambodia/">
                     <FontAwesomeIcon icon={faFacebookF} />
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="#">
+                  <a href="https://www.youtube.com/channel/UCq_XVx9Ov7TpTCYh-gGRnwg">
                     <FontAwesomeIcon icon={faYoutube} />
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="#">
+                  <a href="https://www.linkedin.com/company/credit-bureau-cambodia/posts/'">
                     <FontAwesomeIcon icon={faLinkedin} />
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="#">
-                    <FontAwesomeIcon icon={faInstagram} />
+                  <a href="https://t.me/Creditbureaucambodia">
+                    <FontAwesomeIcon icon={faTelegram} />
                   </a>
                 </li>
               </ul>
@@ -113,76 +114,71 @@ const Footer = () => {
               <p className="mt-3">&copy; 2024 CBC. All rights reserved.</p>
             </Col>
 
-            {/* Browse Courses */}
+            {/* Our Products */}
             <Col xs={6} md={2} className="mb-4 mb-md-0">
               <h5 className="text-uppercase text-white mb-4 font-weight-bold">
-                {t('browseCourses')}
+                {/* {t('browseCourses')} */}
+                Our Products
               </h5>
               <ul className="list-unstyled  small">
-                <li>
-                  <a href="#">Programming</a>
-                </li>
-                <li>
-                  <a href="#">Learn Economics</a>
-                </li>
-                <li>
-                  <a href="#">Learn Project</a>
-                </li>
-                <li>
-                  <a href="#">Learn Business</a>
-                </li>
-                <li>
-                  <a href="#">Learn Software</a>
-                </li>
-                <li>
-                  <a href="#">Learn Computer</a>
-                </li>
+                <li><a href="https://www.creditbureau.com.kh/b2c-personal-credit-report">Personal Credit Report</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2c-financial-health-check">Financial Health Check</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-credit-report-commercial-credit-report">Commercial Credit Report</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-credit-report-consumer-credit-report">Consumer Credit Report</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2c-k-score">K-Score</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-bi-data-analytics-report">Data Analytic Report</a></li>
+
+                <li><a href="https://www.creditbureau.com.kh/b2b-bi-credit-risk-heat-map">Credit Risk Heat Map</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-portfolio-monitoring-service-portfolio-monitoring-service">Portfolio Monitoring</a></li>
+
+                <li><a href="https://www.creditbureau.com.kh/b2b-customized-solutions">Customized Solution</a></li>
               </ul>
             </Col>
 
-            {/* About Us */}
+            {/* Reports */}
             <Col xs={6} md={2} className="mb-4 mb-md-0">
               <h5 className="text-uppercase text-white mb-4 font-weight-bold">
-                {t('aboutUs')}
+                {/* {t('aboutUs')} */}
+                Reports
               </h5>
               <ul className="list-unstyled  small">
-                <li>
-                  <a href="#">Company Profile</a>
-                </li>
-                <li>
-                  <a href="#">Vision, Mission & Core Value</a>
-                </li>
-                <li>
-                  <a href="#">Shareholders</a>
-                </li>
-                <li>
-                  <a href="#">Board of Directors</a>
-                </li>
-                <li>
-                  <a href="#">Management Team</a>
-                </li>
+                <li><a href="https://www.creditbureau.com.kh/data-for-good-annual-report">Annual Reports</a></li>
+                <li><a href="https://www.creditbureau.com.kh/news-and-event-newsletter">Newsletter</a></li>
+                <li><a href="https://www.creditbureau.com.kh/data-for-good-consumer-credit-index">Consumer Credit Index</a></li>
+                <li><a href="https://www.creditbureau.com.kh/data-for-good-other-reports">Other Reports</a></li>
               </ul>
             </Col>
 
             {/* Media */}
             <Col xs={6} md={2} className="mb-4 mb-md-0">
               <h5 className="text-uppercase text-white mb-4 font-weight-bold">
-                Media
+                CBC Members
               </h5>
-              <ul className="list-unstyled small">
-                <li>
-                  <a href="#">Programming</a>
-                </li>
-                <li>
-                  <a href="#">CSR Activities</a>
-                </li>
-                <li>
-                  <a href="#">Videos and Photos</a>
-                </li>
+              <ul className="list-unstyled  small">
+                <li><a href="https://www.creditbureau.com.kh/null">Benefits</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-our-members-become-member">How to Become Member</a></li>
+                <li><a href="https://www.creditbureau.com.kh/b2b-our-members-member-directory">Member Directory</a></li>
+              </ul>
+            </Col>
+            <Col xs={6} md={2} className="mb-4 mb-md-0">
+              <h5 className="text-uppercase text-white mb-4 font-weight-bold">
+                About Us
+              </h5>
+              <ul className="list-unstyled  small">
+                <li><a href="https://www.creditbureau.com.kh/null">Company Profile</a></li>
+                <li><a href="https://www.creditbureau.com.kh/null">Organizational Chart</a></li>
+                <li><a href="https://www.creditbureau.com.kh/about-cbc-vision">Visions, Missions, Core Value</a></li>
+
+                <li><a href="https://www.creditbureau.com.kh/about-cbc-shareholders">Shareholders</a></li>
+                <li><a href="https://www.creditbureau.com.kh/about-cbc-board-of-directors">Board of Directors </a></li>
+                <li><a href="https://www.creditbureau.com.kh/about-cbc-management-team">Management Team</a></li>
+                <li><a href=" https://www.creditbureau.com.kh/career-current-opportunity">Careers</a></li>
+                <li><a href="https://www.creditbureau.com.kh/news-upcoming-event">Latest News</a></li>
+                <li><a href="https://www.creditbureau.com.kh/about-cbc-csr-and-sustainability">CSR Activities</a></li>
               </ul>
             </Col>
             {/* Our Members */}
-            <Col xs={6} md={2} className="mb-4 mb-md-0">
+            <Col xs={6} md={1} className="mb-4 mb-md-0">
 
               <div>
                 <h5 className="text-uppercase text-white mb-4 font-weight-bold">
