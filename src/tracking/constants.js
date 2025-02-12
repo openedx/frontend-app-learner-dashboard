6a43918b56ff40a26169303e07542d43418778fc
Kira Miller <kira.miller15@yahoo.com>
2025-02-12 20:15:01 +0000

    fix: renaming slot

--- src/tracking/constants.js
+++ src/tracking/constants.js
@@ -20,9 +20,9 @@ export const events = StrictDict({
   leaveSession: 'leaveSession',
   unenrollReason: 'unenrollReason',
   entitlementUnenrollReason: 'entitlementUnenrollReason',
-  enterpriseDashboardModalOpened: 'enterpriseDashboardModalOpened',
-  enterpriseDashboardModalCTAClicked: 'enterpriseDashboardModalCTAClicked',
-  enterpriseDashboardModalClosed: 'enterpriseDashboardModalClosed',
+  dashboardModalOpened: 'dashboardModalOpened',
+  dashboardModalCTAClicked: 'dashboardModalCTAClicked',
+  dashboardModalClosed: 'dashboardModalClosed',
 });
 
 const learnerPortal = 'edx.ui.enterprise.lms.dashboard.learner_portal_modal';
@@ -39,9 +39,9 @@ export const eventNames = StrictDict({
   leaveSession: 'course-dashboard.leave-session',
   unenrollReason: 'unenrollment_reason.selected',
   entitlementUnenrollReason: 'entitlement_unenrollment_reason.selected',
-  enterpriseDashboardModalOpened: `${learnerPortal}.opened`,
-  enterpriseDashboardModalCTAClicked: `${learnerPortal}.dashboard_cta.clicked`,
-  enterpriseDashboardModalClosed: `${learnerPortal}.closed`,
+  dashboardModalOpened: `${learnerPortal}.opened`,
+  dashboardModalCTAClicked: `${learnerPortal}.dashboard_cta.clicked`,
+  dashboardModalClosed: `${learnerPortal}.closed`,
   findCoursesClicked: 'edx.bi.dashboard.find_courses_button.clicked',
   purchaseCredit: 'edx.bi.credit.clicked_purchase_credit',
   filterClicked: 'course-dashboard.filter.clicked',
