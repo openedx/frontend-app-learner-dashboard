3ea088e411c6709d17f56f1f3488fcae34b98318
Jason Wesson <jsnwesson@gmail.com>
2025-03-03 10:29:12 -0800

    Revert "fix: PR requests"
    This reverts commit 76783133dae9e7d64a0c42701993fdc47425d80b.

--- src/plugin-slots/DashboardModalSlot/index.jsx
+++ src/plugin-slots/DashboardModalSlot/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 
-const DashboardModalSlot = () => (
+const DashboardModal = () => (
   <PluginSlot id="dashboard_modal_slot" />
 );
 
-export default DashboardModalSlot;
+export default DashboardModal;
