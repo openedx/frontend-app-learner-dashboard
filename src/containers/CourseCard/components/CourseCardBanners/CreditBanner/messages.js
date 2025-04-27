fb86c9c1d2993e1138628f74e82838181dff43ff
Adolfo R. Brandes <adolfo@axim.org>
2025-04-27 13:09:47 -0300

    fix: Add missing message descriptions

--- src/containers/CourseCard/components/CourseCardBanners/CreditBanner/messages.js
+++ src/containers/CourseCard/components/CourseCardBanners/CreditBanner/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   error: {
     id: 'learner-dash.courseCard.banners.credit.error',
-    description: '',
+    description: 'Error message for credit transaction with support email link',
     defaultMessage: 'An error occurred with this transaction.  For help, contact {supportEmailLink}.',
   },
   errorNoEmail: {
     id: 'learner-dash.courseCard.banners.credit.errorNoEmail',
-    description: '',
+    description: 'Error message for credit transaction without support email',
     defaultMessage: 'An error occurred with this transaction.',
   },
 });
