a9194261c809471d318baac64e7772a0e757aef1
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-05-13 08:12:06 -0700

    chore: Replace query-string with URLSearchParams (#613)
    Co-authored-by: diana-villalvazo-wgu <dianaximena.villalva@wgu.edu>

--- src/data/services/lms/utils.js
+++ src/data/services/lms/utils.js
@@ -1,5 +1,33 @@
-import queryString from 'query-string';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+
+/**
+ * stringify(query, existingQuery)
+ * simple wrapper to convert an object to a query string
+ * @param {object} query - object to convert
+ * @param {string} existingQuery - existing query string
+ * @returns {string} - query string
+ */
+
+export const stringify = (query, existingQuery = '') => {
+  const searchParams = new URLSearchParams(existingQuery);
+
+  Object.entries(query).forEach(([key, value]) => {
+    if (value === undefined || value === null || value === '') {
+      searchParams.delete(key);
+    } else if (Array.isArray(value)) {
+      searchParams.delete(key);
+      value.forEach((val) => {
+        if (val !== undefined && val !== null && val !== '') {
+          searchParams.append(key, val);
+        }
+      });
+    } else {
+      searchParams.set(key, value);
+    }
+  });
+
+  return searchParams.toString();
+};
 
 /**
  * get(url)
@@ -10,21 +38,23 @@ export const get = (...args) => getAuthenticatedHttpClient().get(...args);
 /**
  * post(url, data)
  * simple wrapper providing an authenticated Http client post action
- * queryString.stringify is used to convert the object to query string with = and &
+ * stringify is used to convert the object to query string with = and &
  * @param {string} url - target url
  * @param {object|string} body - post payload
  */
-export const post = (url, body) => getAuthenticatedHttpClient().post(url, queryString.stringify(body));
+export const post = (url, body) => getAuthenticatedHttpClient().post(url, stringify(body));
 
 export const client = getAuthenticatedHttpClient;
 
 /**
  * stringifyUrl(url, query)
- * simple wrapper around queryString.stringifyUrl that sets skip behavior
+ * simple wrapper to convert a url and query object to a full url
  * @param {string} url - base url string
  * @param {object} query - query parameters
+ * @returns {string} - full url
  */
-export const stringifyUrl = (url, query) => queryString.stringifyUrl(
-  { url, query },
-  { skipNull: true, skipEmptyString: true },
-);
+export const stringifyUrl = (url, query) => {
+  const [baseUrl, existingQuery = ''] = url.split('?');
+  const queryString = stringify(query, existingQuery);
+  return queryString ? `${baseUrl}?${queryString}` : baseUrl;
+};
