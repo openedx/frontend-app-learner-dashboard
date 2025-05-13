5562d8a339ece5be280d5a4b84f02fb422b2a888
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-05-13 08:23:20 -0700

    chore: Clean up learnerDashboardHeader files (#614)

--- src/containers/LearnerDashboardHeader/hooks.js
+++ src/containers/LearnerDashboardHeader/hooks.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useWindowSize, breakpoints } from '@openedx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import track from 'tracking';
 import { StrictDict } from 'utils';
@@ -7,24 +6,12 @@ import { linkNames } from 'tracking/constants';
 
 import getLearnerHeaderMenu from './LearnerDashboardMenu';
 
-import * as module from './hooks';
-
 export const state = StrictDict({
   isOpen: (val) => React.useState(val), // eslint-disable-line
 });
 
-export const useIsCollapsed = () => {
-  const { width } = useWindowSize();
-  const isCollapsed = React.useMemo(() => (width <= breakpoints.large.minWidth), [width]);
-  return isCollapsed;
-};
-
 export const findCoursesNavClicked = (href) => track.findCourses.findCoursesClicked(href, {
   linkName: linkNames.learnerHomeNavExplore,
-});
-
-export const findCoursesNavDropdownClicked = (href) => track.findCourses.findCoursesClicked(href, {
-  linkName: linkNames.learnerHomeNavDropdownExplore,
 });
 
 export const useLearnerDashboardHeaderMenu = ({
@@ -34,20 +21,7 @@ export const useLearnerDashboardHeaderMenu = ({
   return getLearnerHeaderMenu(formatMessage, courseSearchUrl, authenticatedUser, exploreCoursesClick);
 };
 
-export const useLearnerDashboardHeaderData = () => {
-  const [isOpen, setIsOpen] = module.state.isOpen(false);
-  const toggleIsOpen = () => setIsOpen(!isOpen);
-
-  return {
-    isOpen,
-    toggleIsOpen,
-  };
-};
-
 export default {
-  useIsCollapsed,
   findCoursesNavClicked,
-  findCoursesNavDropdownClicked,
-  useLearnerDashboardHeaderData,
   useLearnerDashboardHeaderMenu,
 };
