4d25f6f0c3f0ad69b86666e2da9da60de2c6201f
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-06-12 12:04:09 -0600

    test: remove use of shallow

--- src/containers/UnenrollConfirmModal/components/ConfirmPane.test.jsx
+++ src/containers/UnenrollConfirmModal/components/ConfirmPane.test.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import { shallow } from '@edx/react-unit-test-utils';
+import { render, screen } from '@testing-library/react';
+import { formatMessage } from 'testUtils';
+import { IntlProvider } from 'react-intl';
 
 import { ConfirmPane } from './ConfirmPane';
+import messages from './messages';
+
+jest.unmock('@edx/frontend-platform/i18n');
+jest.unmock('@openedx/paragon');
+jest.unmock('react');
 
 describe('UnenrollConfirmModal ConfirmPane', () => {
-  test('snapshot', () => {
+  it('renders title', () => {
     const props = {
       handleClose: jest.fn().mockName('props.handleClose'),
       handleConfirm: jest.fn().mockName('props.handleConfirm'),
     };
-    expect(shallow(<ConfirmPane {...props} />).snapshot).toMatchSnapshot();
+    render(<IntlProvider locale="en"><ConfirmPane {...props} /></IntlProvider>);
+    const header = screen.getByText(formatMessage(messages.confirmHeader));
+    expect(header).toBeInTheDocument();
+  });
+  it('renders cancel button', () => {
+    const props = {
+      handleClose: jest.fn().mockName('props.handleClose'),
+      handleConfirm: jest.fn().mockName('props.handleConfirm'),
+    };
+    render(<IntlProvider locale="en"><ConfirmPane {...props} /></IntlProvider>);
+    const cancelButton = screen.getByRole('button', { name: formatMessage(messages.confirmCancel) });
+    expect(cancelButton).toBeInTheDocument();
+  });
+  it('renders unenroll button', () => {
+    const props = {
+      handleClose: jest.fn().mockName('props.handleClose'),
+      handleConfirm: jest.fn().mockName('props.handleConfirm'),
+    };
+    render(<IntlProvider locale="en"><ConfirmPane {...props} /></IntlProvider>);
+    const unenrollButton = screen.getByRole('button', { name: formatMessage(messages.confirmUnenroll) });
+    expect(unenrollButton).toBeInTheDocument();
   });
 });
