668049a355527d37a6c90e3328c11bbee2eb08f5
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-06-17 18:22:48 -0600

    fix: import intlProvider from frontend-platform

--- src/containers/EmailSettingsModal/index.test.jsx
+++ src/containers/EmailSettingsModal/index.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
 
 import hooks from './hooks';
 import EmailSettingsModal from '.';
