3b8340187a012812bf2abfd59e0e6d315062c47c
MaxFrank13 <mfrank@2u.com>
2025-11-26 17:25:35 +0000

    fix: test coverage

--- src/containers/ProgramDashboard/api.ts
+++
@@ -1,8 +0,0 @@
-import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
-import { getConfig } from '@edx/frontend-platform';
-
-export async function getProgramsListData() {
-  const url = `${getConfig().LMS_BASE_URL}/api/dashboard/v0/programs/`;
-  const response = await getAuthenticatedHttpClient().get(url);
-  return response;
-}
